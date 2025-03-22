103b9157bba4c722d89aea5d36b8df6167338214
Hitesh-xe <hiteshkanisetty5@gmail.com>
2025-03-22 06:21:32 +0530

    final

--- controllers/admin.js
+++ controllers/admin.js
@@ -31,22 +31,13 @@ exports.getform = (req, res, next) => {
   });
 };
 exports.postformgm = (req, res, next) => {
-  const {
-    firstname,
-    lastname,
-    mobile,
-    age,
-    gender,
-    employeeid,
-    layoutname,
-    plotssold,
-  } = req.body;
-  const image = req.file;
+  const { firstname, lastname, mobile, age, gender, employeeid } = req.body;
+  const image = req.filePath;
   Gmmodel.find({ employeeid: employeeid }).then((result) => {
     if (result.length > 0) {
       return res.status(400).json({ message: "User Already Exists" });
     } else {
-      const imageurl = image.path;
+      const imageurl = image.filePath;
       const gmmodel = new Gmmodel({
         firstname: firstname,
         lastname: lastname,
@@ -54,9 +45,7 @@ exports.postformgm = (req, res, next) => {
         age: age,
         gender: gender,
         employeeid: employeeid,
-        layoutname: layoutname,
-        plotssold: plotssold,
-        image: imageurl,
+        image: image,
       });
       gmmodel.save().then((result) => {
         console.log("User Created");
@@ -66,18 +55,9 @@ exports.postformgm = (req, res, next) => {
   });
 };
 exports.postformba = (req, res, next) => {
-  const {
-    firstname,
-    lastname,
-    mobile,
-    age,
-    gender,
-    employeeid,
-    layoutname,
-    plotssold,
-    guidance,
-  } = req.body;
-  const image = req.file;
+  const { firstname, lastname, mobile, age, gender, employeeid, guidance } =
+    req.body;
+  const image = req.filePath;
   Bussmodel.find({ employeeid: employeeid }).then((result) => {
     if (result.length > 0) {
       return res.status(400).json({ message: "User Already Exists" });
@@ -89,7 +69,7 @@ exports.postformba = (req, res, next) => {
       ).then((result) => {
         console.log("Associates Updated");
       });
-      const imageurl = image.path;
+
       const bussmodel = new Bussmodel({
         firstname: firstname,
         lastname: lastname,
@@ -97,10 +77,8 @@ exports.postformba = (req, res, next) => {
         age: age,
         gender: gender,
         employeeid: employeeid,
-        layoutname: layoutname,
-        plotssold: plotssold,
         guidance: guidance,
-        image: imageurl,
+        image: image,
       });
       bussmodel.save().then((result) => {
         console.log("User Created");
@@ -111,30 +89,43 @@ exports.postformba = (req, res, next) => {
 };
 
 exports.getgmdata = (req, res, next) => {
-  Gmmodel.find().then((result) => {
-    res.render("board-manager/view-gm.ejs", {
-      pageTitle: "Login",
-      title: "General Managers",
-      type: "general-manager",
-      employees: result,
+  setTimeout(() => {
+    Gmmodel.find().then((result) => {
+      res.render("board-manager/view-gm.ejs", {
+        pageTitle: "Login",
+        title: "General Managers",
+        type: "general-manager",
+        employees: result,
+      });
     });
-  });
+  }, 1000);
 };
 exports.getbadata = (req, res, next) => {
-  Bussmodel.find().then((result) => {
-    res.render("board-manager/view-gm.ejs", {
-      pageTitle: "Login",
-      title: "Bussiness Associates",
-      type: "bussiness-associate",
-      employees: result,
+  setTimeout(() => {
+    Bussmodel.find().then((result) => {
+      res.render("board-manager/view-gm.ejs", {
+        pageTitle: "Login",
+        title: "Bussiness Associates",
+        type: "bussiness-associate",
+        employees: result,
+      });
     });
-  });
+  }, 1000);
 };
+// exports.getbm = async (req, res, next) => {
+//   const id = req.query.id;
+//   console.log("Query ID:", id);
+
+//   Bussmodel.find({ employeeid: id }).then((result) => {
+//     console.log(result);
+//     res.json({ products: result });
+//   });
+// };
 exports.getbm = async (req, res, next) => {
   const id = req.query.id;
   console.log("Query ID:", id);
 
-  Bussmodel.find({ employeeid: id }).then((result) => {
+  Bussmodel.find({ guidance: id }).then((result) => {
     console.log(result);
     res.json({ products: result });
   });
@@ -156,23 +147,27 @@ exports.getbm = async (req, res, next) => {
 
 exports.getselectedgm = (req, res, next) => {
   const id = req.query.id;
-  Gmmodel.findById({ _id: id }).then((data) => {
-    res.render("board-manager/dispgm.ejs", {
-      type: "gm",
-      employee: data,
+  setTimeout(() => {
+    Gmmodel.findById({ _id: id }).then((data) => {
+      res.render("board-manager/dispgm.ejs", {
+        type: "gm",
+        employee: data,
+      });
     });
-  });
+  }, 1000);
 };
 exports.getselectedba = (req, res, next) => {
   const id = req.query.id;
-  Bussmodel.findById({ _id: id }).then((data) => {
-    res.render("board-manager/dispgm.ejs", {
-      employee: data,
-      type: "ba",
+  setTimeout(() => {
+    Bussmodel.findById({ _id: id }).then((data) => {
+      res.render("board-manager/dispgm.ejs", {
+        employee: data,
+        type: "ba",
+      });
     });
-  });
+  }, 1000);
 };
-exports.postdelete = (req, res, next) => {
+exports.postgmdelete = (req, res, next) => {
   const id = req.body.id;
   Gmmodel.deleteOne({ _id: id })
     .then((result) => {
@@ -183,33 +178,47 @@ exports.postdelete = (req, res, next) => {
       res.status(500).json({ message: "Internal server error" });
     });
 };
+exports.postbadelete = (req, res, next) => {
+  const id = req.body.id;
+  Bussmodel.deleteOne({ _id: id })
+    .then((result) => {
+      res.redirect("/view-ba");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+};
 
 exports.getedit = (req, res, next) => {
   const id = req.params.id;
-  const edit = req.query.edit;
-  Gmmodel.findById(id)
-    .then((data) => {
-      res.render("board-manager/edit-data.ejs", {
-        editing: edit,
-        gmdata: data,
-      });
-    })
-    .catch((err) => console.log(err));
+  const type = req.query.type;
+
+  if (type === "gm") {
+    Gmmodel.findById(id)
+      .then((data) => {
+        res.render("board-manager/edit-data.ejs", {
+          editing: true,
+          gmdata: data,
+          type: "gm",
+        });
+      })
+      .catch((err) => console.log(err));
+  } else if (type === "ba") {
+    Bussmodel.findById(id)
+      .then((data) => {
+        res.render("board-manager/edit-data.ejs", {
+          editing: true,
+          gmdata: data,
+          type: "ba",
+        });
+      })
+      .catch((err) => console.log(err));
+  }
 };
 
 exports.postgmedit = (req, res, next) => {
-  const {
-    id,
-    firstname,
-    lastname,
-    mobile,
-    age,
-    gender,
-    employeeid,
-    layoutname,
-    plotssold,
-  } = req.body;
-
+  const { id, firstname, lastname, mobile, age, gender, employeeid } = req.body;
   Gmmodel.findById(id)
     .then((gm) => {
       if (!gm) {
@@ -221,13 +230,81 @@ exports.postgmedit = (req, res, next) => {
       gm.mobile = mobile;
       gm.gender = gender;
       gm.employeeid = employeeid;
-      gm.layoutname = layoutname;
-      gm.plotssold = plotssold;
 
       return gm.save().then((result) => {
         console.log("updated product");
-        res.redirect("/");
+        res.redirect("/view-gm");
       });
     })
     .catch((err) => console.log(err));
+};
+
+exports.postbaedit = (req, res, next) => {
+  const { id, firstname, lastname, mobile, age, gender, employeeid, guidance } =
+    req.body;
+  Bussmodel.findById(id)
+    .then((ba) => {
+      if (!ba) {
+        return res.redirect("/view-ba");
+      }
+      ba.firstname = firstname;
+      ba.lastname = lastname;
+      ba.age = age;
+      ba.mobile = mobile;
+      ba.gender = gender;
+      ba.employeeid = employeeid;
+      ba.guidance = guidance;
+
+      return ba.save().then((result) => {
+        console.log("updated product");
+        res.redirect("/view-ba");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+exports.postaddlayout = (req, res, next) => {
+  const { id, type } = req.query;
+  const { layoutName, plotsSold } = req.body;
+
+  if (type === "gm") {
+    Gmmodel.findOneAndUpdate(
+      { _id: id },
+      {
+        $push: {
+          layouts: {
+            name: layoutName,
+            plotsSold: plotsSold,
+          },
+        },
+      },
+      { new: true }
+    )
+      .then((result) => {
+        res.redirect(`/general-manager?id=${id}`);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error" });
+      });
+  } else if (type === "ba") {
+    Bussmodel.findOneAndUpdate(
+      { _id: id },
+      {
+        $push: {
+          layouts: {
+            name: layoutName,
+            plotsSold: plotsSold,
+          },
+        },
+      },
+      { new: true }
+    )
+      .then((result) => {
+        res.redirect(`/bussiness-associate?id=${id}`);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error" });
+      });
+  }
 };
--- controllers/general.js
+++ controllers/general.js
@@ -1,16 +1,28 @@
 const Gmmodel = require("../models/gmmodel");
 const Bussmodel = require("../models/bussmodel");
+const fs = require("fs");
+const path = require("path");
 exports.getgminterface = (req, res, next) => {
-  const id = req.body.id;
-  const password = req.body.password;
-  if (password === "webcap") {
-    Gmmodel.findOne({ employeeid: id }).then((data) => {
-      res.render("general-manager/gminterface.ejs", {
-        employee: data,
-        associate: true,
-      });
-    });
-  }
+  const { id, password } = req.body;
+
+  Gmmodel.findOne({ employeeid: id })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ message: "Authentication failed" });
+      }
+
+      // Assuming passwords are stored in plain text for simplicity
+      if (password === "webcap") {
+        res.render("general-manager/gminterface.ejs", {
+          employee: user,
+          associate: true,
+          isAuthenticated: true,
+        });
+      } else {
+        res.status(401).json({ message: "Authentication failed" });
+      }
+    })
+    .catch((err) => console.log(err));
 };
 exports.getgm = (req, res, next) => {
   const id = req.query.id;
@@ -18,6 +30,7 @@ exports.getgm = (req, res, next) => {
     res.render("general-manager/gminterface.ejs", {
       employee: data,
       associate: true,
+      isAuthenticated: true,
     });
   });
 };
@@ -29,6 +42,7 @@ exports.getassociates = (req, res, next) => {
       associates: data,
       qid: id,
       param: paramid,
+      isAuthenticated: true,
     });
   });
 };
@@ -38,6 +52,7 @@ exports.getdocform = (req, res, next) => {
     res.render("general-manager/doc.ejs", {
       employee: data,
       type: "General-manager",
+      isAuthenticated: true,
     });
   });
 };
@@ -62,4 +77,40 @@ exports.getdocuments = async (req, res, next) => {
   Gmmodel.findOne({ _id: id }).then((result) => {
     res.json({ docs: result.uploaddocs });
   });
+};
+
+exports.docdelete = async (req, res) => {
+  const { id, docId } = req.query;
+
+  try {
+    const user = await Gmmodel.findOne({ _id: id });
+    const doc = user.uploaddocs.id(docId);
+
+    if (doc) {
+      const imagePath = path.join(__dirname, "..", doc.doc);
+      fs.unlink(imagePath, (err) => {
+        if (err) {
+          console.error(`Failed to delete image file: ${err}`);
+          return res.json({ success: false });
+        }
+
+        Gmmodel.updateOne(
+          { _id: id },
+          { $pull: { uploaddocs: { _id: docId } } }
+        )
+          .then(() => {
+            res.json({ success: true });
+          })
+          .catch((error) => {
+            console.error(error);
+            res.json({ success: false });
+          });
+      });
+    } else {
+      res.json({ success: false, message: "Document not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false });
+  }
 };
